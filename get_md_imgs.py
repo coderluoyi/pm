457e2eb111d56f4ca4946eec1f27c03013ddd706
Mki <10372291+luoyi_dev@user.noreply.gitee.com>
2024-05-23 12:04:47 +0000

    add get_md_imgs.py.
    Signed-off-by: Mki <10372291+luoyi_dev@user.noreply.gitee.com>

---
+++ get_md_imgs.py
@@ -0,0 +1,31 @@
+import re
+import requests
+from PIL import Image
+from io import BytesIO
+
+# todo 获取 md 中的图片
+
+file = open("README.md", "r")
+
+pattern = r'http[s]{0,1}://pic[a-z0-9.]*/\d+/(?P<img>.*)(?=\))'
+
+lines = file.readlines()
+
+for line in lines:
+    match = re.search(pattern, line)
+    if match:
+        url = match.group(0)
+        img_name = match.group("img")
+
+        response = requests.get(url)
+        if response.status_code == 200:
+            image_data = response.content
+
+            image = Image.open(BytesIO(image_data))
+
+            # 保存图片为JPG格式
+            image.save(img_name, 'JPEG')
+        else:
+            print('error.code:', response.status_code)
+
+    continue

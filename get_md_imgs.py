bd82da94dccf1903a5576d3ef8cb99d07c91649e
罗一 <794593170@qq.com>
2024-05-24 16:27:27 +0800

    update @2024-05-24 16:27

--- get_md_imgs.py
+++ get_md_imgs.py
@@ -7,9 +7,7 @@ from io import BytesIO
 os.makedirs(exist_ok=True, name='./data/')
 
 # todo 获取 md 中的图片
-
-# file = open("README.md", "r")
-file = open("README2.md", "r")
+file = open("README.md", "r")
 
 pattern = r'http[s]{0,1}://pic[a-z0-9.]*/\d+/(?P<img>.*)(?=\))'
 
